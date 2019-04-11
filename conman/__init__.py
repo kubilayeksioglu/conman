9d9d4187dc3961511ecfc6d678130a55a1c19c3f
Kubilay Eksioglu <kubilayeksioglu@gmail.com>
2019-04-11 19:53:38 +0300

    Simplify ConmanContainer API
    Now ConmanContainer uses engines, which will allow running in the Kubernetes or Docker

--- conman/__init__.py
+++ conman/__init__.py
@@ -4,7 +4,153 @@
 
 __author__ = """Kubilay Eksioglu"""
 __email__ = 'kubilayeksioglu@gmail.com'
-__version__ = '0.1.9'
+__version__ = '0.2.0'
 
 
-from conman.conman import ConmanContainer
+import logging
+import docker
+from docker.errors import NotFound as DockerNotFound
+logger = logging.getLogger(__name__)
+
+
+class ContainerNotRunning(Exception):
+    pass
+
+
+class DockerEngine:
+
+    def __init__(self):
+        self._client = docker.from_env()
+
+    def run(self, name, image, ports=None, expose=None, command=None, volumes={}, network=None):
+        if network is not None:
+            try:
+                _ = self._client.networks.get(network)
+            except DockerNotFound:
+                self._client.networks.create(network, driver="bridge")
+
+
+        self._client.containers.run(image, 
+                                    stdin_open=True, 
+                                    tty=True, 
+                                    command="/bin/sh", 
+                                    ports=ports, 
+                                    name=name, 
+                                    detach=True,
+                                    network=network, 
+                                    volumes=volumes)
+
+        # send a default command, in case
+        if command:
+            container = self.get(name)
+            self.exec(container, command)
+
+    def exec(self, container, command, output=False):
+        """
+        Send a bash command to the container
+
+        :param command: str     Command to be run
+        :param output: bool     Whether Conman should wait for the output or detach immediately.
+        :return: None if output=False, (exit code: int, msg: str) pairs otherwise
+        """
+        if not output:
+            container.exec_run("sh -c '%s'" % command, detach=True)
+            return None
+
+        exit_code, stream_msgs = container.exec_run("sh -c '%s'" % command, stream=True)
+        return exit_code, [msg.decode('utf-8') for msg in stream_msgs]
+
+    def get(self, name):
+        try:
+            container = self._client.containers.get(name)
+        except DockerNotFound:
+            return None
+
+        # if container is exited, we simply remove it
+        if container.status == 'exited':
+            container.remove()
+            return None
+        
+        return container
+
+    def stop(self, container):
+        container.stop()
+        container.remove()
+
+    def inspect(self, container):
+        client = docker.APIClient(base_url='unix://var/run/docker.sock')
+        inspection = client.inspect_container(container.id)
+        client.close()
+
+        return inspection
+
+    def __del__(self):
+        self._client.close()
+
+
+class ConmanContainer:
+
+    def __init__(self, engine, name, engine_params={}):
+        self.engine = engine(**engine_params)
+        self.name = name
+
+    @property
+    def container(self):
+        container = self.engine.get(self.name)
+        if container:
+            return container
+        else:
+            raise ContainerNotRunning()
+
+    def start(self, image, **kwargs):
+        container = self.engine.get(self.name)
+        if container:
+            logger.warning("Container %s is already running" % self.name)
+            return
+
+        self.engine.run(self.name, image, **kwargs)
+
+    def status(self):
+        return self.engine.status(self.container)
+
+    def stop(self):
+        self.engine.stop(self.container)
+
+    def exec(self, command, output=True):
+        return self.engine.exec(self.container, command, output=output)
+
+    def inspect(self):
+        return self.engine.inspect(self.container)
+
+
+RUN_CONFIG_KEYS = ['ports', 'command', 'expose', 'volumes', 'network']
+
+
+class TemplatedContainer(ConmanContainer):
+
+    image = None
+    name = None
+    name_template = None
+    ports = None
+    command = None
+    expose = None
+    volumes = {}
+    network = None
+
+    def __init__(self, engine, id=None, name=None, engine_params={}, **kwargs):
+        if name is None:
+            if id:
+                name = self.name_template % id
+            else:
+                name = self.name
+        if name is None:
+            raise RuntimeError("cls.name, id or name should be provided")
+        super().__init__(engine, name, engine_params)
+        
+        for key in ['image'] + RUN_CONFIG_KEYS:
+            setattr(self, key, kwargs.get(key, getattr(self.__class__, key)))
+
+    def start(self, image=None, **kwargs):
+        runconfig = {k:getattr(self, k) for k in RUN_CONFIG_KEYS}
+        image = image if image else self.image
+        super().start(image, **runconfig)
--- tests/test_conman.py
+++ tests/test_conman.py
@@ -5,20 +5,31 @@
 import os
 import unittest
 
-import docker
-
-from conman import ConmanContainer
-from conman.utils import get_ip
+from conman import TemplatedContainer, DockerEngine, ContainerNotRunning
 
 TESTS_DIR = os.path.dirname(__file__)
 
 
-class GznodeContainer(ConmanContainer):
-    image = "197358733965.dkr.ecr.eu-west-1.amazonaws.com/ride/ros-gazebo:v1.0"
-    container_name = "test-gznode"
-    local_ports = (8080,)
-    default_command = None
+CONMAN_TEST_PYTHON_NETWORK = "conman-test-python-network"
 
+
+class PythonContainer(TemplatedContainer):
+    image = "python:3.7-slim"
+    ports = {'8080':'8080'}
+    command = "python3 -m http.server 8080"
+    name = "conman-test-python"
+
+class NetworkPythonContainer(TemplatedContainer):
+    image = "python:3.7-slim"
+    ports = {'8081':'8081'}
+    network = "conmantest"
+    command = "python3 -m http.server 8081"
+    name = CONMAN_TEST_PYTHON_NETWORK
+
+class WgetContainer(TemplatedContainer):
+    image = "inutano/wget:latest"
+    network = "conmantest"
+    name = "conman-test-wget-network"
 
 class TestConman(unittest.TestCase):
     """Tests for `conman` package."""
@@ -27,68 +38,99 @@ class TestConman(unittest.TestCase):
         """Set up test fixtures, if any."""
         # create dummy image with docker-py
 
+    def _stop(self, container):
+        try:
+            container.stop()
+        except ContainerNotRunning:
+            pass
+
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        self._stop(PythonContainer(DockerEngine))
+        self._stop(NetworkPythonContainer(DockerEngine))
+        self._stop(WgetContainer(DockerEngine))
 
-    def test_default_ip(self):
+    def test_port(self):
         """Test when no ip provided, does container receive correct host address (0.0.0.0)."""
-        container = GznodeContainer(0)
+        container = PythonContainer(DockerEngine)
         container.start()
-        address = container.get_host_address(8080)
-        expected = '0.0.0.0:8080'
-        self.assertEqual(address, expected, "Default IP is incorrect. Expected: %s Actual: %s")
+        inspection = container.inspect()
+
+        expected = [{"HostIp": "0.0.0.0", "HostPort": "8080"}]
+        port_info = inspection['NetworkSettings']['Ports'].get('8080/tcp')
+        self.assertEqual(port_info, expected, "Port is different than what is expected")
+
+        # expected = '0.0.0.0:8080'
+        # self.assertEqual(address, expected, "Default IP is incorrect. Expected: %s Actual: %s")
         container.stop()
 
-    def test_autodetect_port(self):
-        """
-        Test if detecting port automatically works as expected
-        :return:
-        """
-        container = GznodeContainer(1)
-        container.start()
-        address = container.get_host_address(8080)
-        expected = '0.0.0.0:8081'
-        self.assertEqual(address, expected, "Auto-detected port is wrong. Expected: %s Actual: %s")
-        container.stop()
+    def test_network(self):
+        """Test when no ip provided, does container receive correct host address (0.0.0.0)."""
+        python_container = NetworkPythonContainer(DockerEngine, name=CONMAN_TEST_PYTHON_NETWORK)
+        python_container.start()
 
-    def test_get_host_address(self):
-        """
-        Test if an IP is provided, can ConmanContainer bind that to IP address instead of 0.0.0.0
-        :return:
-        """
-        local_ip = get_ip()
+        wget_container = WgetContainer(DockerEngine)
+        wget_container.start()
 
-        container = GznodeContainer(1)
-        container.start(local_ip)
-        address = container.get_host_address(8080)
-        expected = '%s:8081' % local_ip
-        self.assertEqual(address, expected, "Host address is wrong. Expected: %s Actual: %s")
-        container.stop()
+        error_code, lines = wget_container.exec('wget -q -O - \"$@\" http://%s:8081' % CONMAN_TEST_PYTHON_NETWORK, output=True)
+        self.assertTrue(lines[0].startswith('<!DOCTYPE HTML PUBLIC'))
 
-    def test_volume(self):
-        """
-        Test if an IP is provided, can ConmanContainer bind that to IP address instead of 0.0.0.0
-        :return:
-        """
-        source_path = os.path.abspath(TESTS_DIR)
+        # expected = '0.0.0.0:8080'
+        # self.assertEqual(address, expected, "Default IP is incorrect. Expected: %s Actual: %s")
+        python_container.stop()
+        wget_container.stop()
 
-        container = GznodeContainer(1)
-        container.volumes = {source_path: {
-            'bind': '/colab',
-            'mode': 'rw'
-        }}
-        container.start()
 
-        client = docker.APIClient(base_url='unix://var/run/docker.sock')
-        inspection = client.inspect_container(container.container.id)
+    # def test_autodetect_port(self):
+    #     """
+    #     Test if detecting port automatically works as expected
+    #     :return:
+    #     """
+    #     container = GznodeContainer(1)
+    #     container.start()
+    #     address = container.get_host_address(8080)
+    #     expected = '0.0.0.0:8081'
+    #     self.assertEqual(address, expected, "Auto-detected port is wrong. Expected: %s Actual: %s")
+    #     container.stop()
 
-        container.stop()
-        client.close()
+    # def test_get_host_address(self):
+    #     """
+    #     Test if an IP is provided, can ConmanContainer bind that to IP address instead of 0.0.0.0
+    #     :return:
+    #     """
+    #     local_ip = get_ip()
 
-        expected = source_path
-        actual = inspection['Mounts'][0]['Source']
-        self.assertEqual(actual, expected, "Host address is wrong. Expected: %s Actual: %s" % (actual, expected))
+    #     container = GznodeContainer(1)
+    #     container.start(local_ip)
+    #     address = container.get_host_address(8080)
+    #     expected = '%s:8081' % local_ip
+    #     self.assertEqual(address, expected, "Host address is wrong. Expected: %s Actual: %s")
+    #     container.stop()
+
+    # def test_volume(self):
+    #     """
+    #     Test if an IP is provided, can ConmanContainer bind that to IP address instead of 0.0.0.0
+    #     :return:
+    #     """
+    #     source_path = os.path.abspath(TESTS_DIR)
+
+    #     container = GznodeContainer(1)
+    #     container.volumes = {source_path: {
+    #         'bind': '/colab',
+    #         'mode': 'rw'
+    #     }}
+    #     container.start()
+
+    #     client = docker.APIClient(base_url='unix://var/run/docker.sock')
+    #     inspection = client.inspect_container(container.container.id)
+
+    #     container.stop()
+    #     client.close()
+
+    #     expected = source_path
+    #     actual = inspection['Mounts'][0]['Source']
+    #     self.assertEqual(actual, expected, "Host address is wrong. Expected: %s Actual: %s" % (actual, expected))
 
 
 if __name__ == "__main__":
